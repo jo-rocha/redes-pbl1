e3140c34113505204cc8f5a5c66a9aa6ceb6ad10
João Pedro O. Rocha <jope.orocha@gmail.com>
2022-06-07 23:04:53 -0300

    interface caminhao atualizacao

--- truck.py
+++ truck.py
@@ -9,13 +9,13 @@ import threading
 # from pbl1.truck import organize_tcan_list
 
 currentLoad = 0
-loadCapacity = 500
-tcanList = None
+loadCapacity = 150
+tcanList = []
 
 clientID = 'truck'
 
 ##MQTT START##
-broker = 'localhost'
+broker = 'mqtt.eclipseprojects.io'
 port = 1883
 client = mqtt.Client()
 
@@ -36,7 +36,7 @@ def on_connect(client, userdata, flags, rc):
     
     # O subscribe fica no on_connect pois, caso perca a conexão ele a renova
     # Lembrando que quando usado o #, você está falando que tudo que chegar após a barra do topico, será recebido
-    client.subscribe("caminhao")
+    client.subscribe("truck")
 
     #publish_thread = threading.Thread
 
@@ -76,14 +76,14 @@ def organize_tcan_list(msg):
     msg = json.loads(msg.decode())
     if msg['header'] == 'cadastro':#cria um dicionário com as informações de cada lixeira
         tcan = {}
-        tcan['id'] = msg['id']
-        tcan['setor'] = msg['setor']
-        tcan['currentLoad'] = msg['value']
+        tcan['id'] = msg['value']['id']
+        tcan['setor'] = msg['value']['setor']
+        tcan['currentLoad'] = msg['value']['value']
         tcanList.append(tcan)
     elif msg['header'] == 'status':#a lixeira recebeu lixo e mandou seu status para o caminhão. O caminhão atualiza a lixeira e faz o resorting da lista
         for i in tcanList:
-            if i['id'] == msg['id']:
-                i['currentLoad'] = msg['value']#atualiza o valor da lixeira na lista do caminhão
+            if i['id'] == msg['value']['id']:
+                i['currentLoad'] = msg['value']['value']#atualiza o valor da lixeira na lista do caminhão
                 tcanList.sort(key = lambda x: int(x['currentLoad']), reverse = True)
             
 #vai pegar a lixeira do topo da lista de prioridade e fazer o processo de ir até a lixeira e a esvaziar        
@@ -91,24 +91,39 @@ def find_execute_route():
     global tcanList
     global currentLoad
     global loadCapacity
+    startConection()
     while True:
         if tcanList:#se a lista nao estiver vazia
             #[interface beginning]
+            time.sleep(5)
             os.system('cls||clear')
-            print(f'[THE TRUCK CURRENT LOAD IS {currentLoad}/{loadCapacity}\nAND IT IS HEADING TOWARDS THE TRASHCAN:{tcanList[0]}\n\n]')
+            goalTcan = tcanList[0]
+            if goalTcan['currentLoad'] == 0:
+                print(f'[THERE ARE NO MORE TRASHCANS TO COLLECT, AND THE TRUCK LOAD IS {currentLoad}/{loadCapacity}]')
+            else:
+                print(f'[THE TRUCK CURRENT LOAD IS {currentLoad}/{loadCapacity}, AND IT IS HEADING TOWARDS THE TRASHCAN:{tcanList[0]}]\n')
             #[interface end]
-            goalTcan = tcanList.pop(0)
-            time.sleep(10)
-            client.publish(goalTcan['setor'], goalTcan['id'])#quando o caminhão chega na lixeira ele publica no tópico da lixeira e o payload é o id da lixeira a ser esvaziada
-            #quando ele esvazia a lixeira ele a coloca de volta no final da lista de lixeiras
-            goalTcan['currentLoad'] = 0
-            tcanList.append(goalTcan)
+            # goalTcan = tcanList.pop(0)
+            aux = int(goalTcan['currentLoad']) + currentLoad
+            if aux >= loadCapacity:
+                print('[THE TRUCK CAN\'T HOLD THIS AMOUNT OF TRASH]\n[UNLOADING AT THE STATION...]')
+                currentLoad = 0 
+                time.sleep(5)
+            else:
+                tcanList.pop(0)#já que eu já pego o topo na linha 100
+                time.sleep(5)
+                client.publish(goalTcan['setor'], goalTcan['id'])#quando o caminhão chega na lixeira ele publica no tópico da lixeira e o payload é o id da lixeira a ser esvaziada
+                #quando ele esvazia a lixeira ele a coloca de volta no final da lista de lixeiras
+                currentLoad += int(goalTcan['currentLoad'])
+                goalTcan['currentLoad'] = 0
+                tcanList.append(goalTcan)
         else:
-            time.sleep(3)
+            time.sleep(5)
             os.system('cls||clear')
             print(f'[NO TRASHCANS REGISTERED IN THE SYSTEM YET]')
 
-message = input('[PRESS ANY KEY TO START THE TRUCK]')
-find_execute_route_thread = threading.Thread(target = find_execute_route)
-find_execute_route_thread.start()
 
+message = input('[PRESS ANY KEY TO START THE TRUCK]')
+find_execute_route()
+# find_execute_route_thread = threading.Thread(target = find_execute_route)
+# find_execute_route_thread.start()
