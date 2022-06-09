82c2c8da5b5b36a567f857d76e32b2ee8b27087f
jo-rocha <jope.orocha@gmail.com>
2022-06-08 21:40:29 -0300

    inicio interface admin

---
+++ admin.py
@@ -0,0 +1,31 @@
+import time
+import os
+import threading
+
+#The idea fo the admin is to have an input interface. The functionalities will be:
+#The admin can input a number, this number will be the number of trashcans to retreieve from the sectors. The sectors will return
+#from the list of all trahcans the most critic ones to amount to the input number.
+#
+#Once the admin has the list of trashcans on his interface he can select one of the numbers on the list, and it will show the admin
+#more information regarding that specific trashcan
+
+def execute_interface():
+    while True:
+        os.system('cls||clear')
+        numberOfTcans = input("[INPUT THE NUMBER OF TRASHCANS YOU WANT TO TRACK:]\n")
+        #manda o request para pegar a lista de lixeiras críticas
+        while True: #aqui eu coloquei um while true imaginando a seguinte lógica: ao mostrar a lista de lixeiras o admin pode selecionar uma lixeira específica
+                    #para mostrar mais dados, só que quando ele sair da tela de uma lixeira específica para não atualizar a lista e ele perder a que já tinha,
+                    #caso ele queira por exemplo olhar os dados de outra lixeira da lista, ele fica em um outro loop de mostra a lista e escolhe específica.
+                    #se ele colocar 'r' para voltar para a tela de pedir um número de lixeiras, o loop é quebrado e ele volta para o loop inicial da interface.
+            os.system('cls||clear')
+            #printa a lista
+            tcanMoreInfo = input("[YOU CAN INPUT THE ID OF ONE TRASHCAN TO SEE MORE INFO, OR PRESS 'R' TO GO BACK TO THE INITIAL SCREEN]")
+            if tcanMoreInfo != 'r':
+                os.system('cls||clear')
+                #print só a lixeira selecionada com mais informações
+                input("[PRESS ANY KEY TO RETURN TO THE PREVIOUS SCREEN]")
+            if tcanMoreInfo == 'r': break
+
+
+execute_interface()
