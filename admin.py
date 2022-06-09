e014d894bad5e9becf1f80c51f1aab032c16c1bf
jo-rocha <jope.orocha@gmail.com>
2022-06-09 00:28:16 -0300

    interface admin

--- admin.py
+++ admin.py
@@ -1,6 +1,8 @@
 import time
 import os
 import threading
+import json
+import requests
 
 #The idea fo the admin is to have an input interface. The functionalities will be:
 #The admin can input a number, this number will be the number of trashcans to retreieve from the sectors. The sectors will return
@@ -14,15 +16,22 @@ def execute_interface():
         os.system('cls||clear')
         numberOfTcans = input("[INPUT THE NUMBER OF TRASHCANS YOU WANT TO TRACK:]\n")
         #manda o request para pegar a lista de lixeiras críticas
+        response = requests.get("http:/localhost:5000/list-tcans")
+        tcanList = json.loads(response.content)
         while True: #aqui eu coloquei um while true imaginando a seguinte lógica: ao mostrar a lista de lixeiras o admin pode selecionar uma lixeira específica
                     #para mostrar mais dados, só que quando ele sair da tela de uma lixeira específica para não atualizar a lista e ele perder a que já tinha,
                     #caso ele queira por exemplo olhar os dados de outra lixeira da lista, ele fica em um outro loop de mostra a lista e escolhe específica.
                     #se ele colocar 'r' para voltar para a tela de pedir um número de lixeiras, o loop é quebrado e ele volta para o loop inicial da interface.
             os.system('cls||clear')
+            print(f'####################################\n')
+            for i in tcanList:
+                print(f'{i["id"]} + {i["setor"]}\n')
+            print(f'####################################\n')
             #printa a lista
-            tcanMoreInfo = input("[YOU CAN INPUT THE ID OF ONE TRASHCAN TO SEE MORE INFO, OR PRESS 'R' TO GO BACK TO THE INITIAL SCREEN]")
+            tcanMoreInfo = input("[YOU CAN SELECT ONE TRASHCAN TO SEE IT'S SPECIFIC DETAILS, OR PRESS 'R' TO GO BACK TO THE INITIAL SCREEN]")
             if tcanMoreInfo != 'r':
                 os.system('cls||clear')
+                print(tcanList[tcanMoreInfo])
                 #print só a lixeira selecionada com mais informações
                 input("[PRESS ANY KEY TO RETURN TO THE PREVIOUS SCREEN]")
             if tcanMoreInfo == 'r': break
--- test/test.py
+++ test/test.py
@@ -6,9 +6,10 @@ rall = rall.replace('\n', '')
 
 sID = 3
 sID2 = 1
+#construção das strings que vão ser inseridas
 auxStr1 = f's{sID}'
 auxStr2 = f's{sID2}'
-#construção das strings que vão ser inseridas
+
 id_tcan = 2
 auxId = f'id-{id_tcan}'
 id_tcan2 = 1
@@ -19,12 +20,13 @@ cl_tcan2 = 0
 auxCl2 = f'cl-{cl_tcan2}'
 #
 
+addTcanString = 
 
 if auxStr1 in rall:
     index = rall.find(auxStr1)
     for index in len(rall):
         if rall[index] == '.':
-            newstring = f';'
+            newstring = f';' + 
             rall = rall[:index] + 
 
 
